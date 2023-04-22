e340cbdc059882b9b5a96867d27183d97e9761d1
Marlon Velasquez <marlonjvelasquez@gmail.com>
2023-04-22 21:07:55 +0200

    Add component Fix image path Add props

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="app">
-    <Upvote msg="Welcome to Your Vue.js App" />
+    <h2 class="text-center text-3xl p-7">UpVote!</h2>
+    <div class="flex justify-center">
+      <Upvote
+        :id="submissions[0].id"
+        :title="submissions[0].title"
+        :avatar="submissions[0].avatar"
+        :description="submissions[0].description"
+        :votes="submissions[0].votes"
+        :submissionImage="submissions[0].submissionImage"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import Upvote from "./components/Upvote.vue"
+import { submissions } from "./data/seed"
 
 export default {
   name: "App",
   components: {
     Upvote,
   },
+  data() {
+    return {
+      submissions,
+    }
+  },
 }
 </script>
 
 <style></style>
+"", "title", "description", "url", "votes", "avatar", "submissionImage"
+
+<!-- :imageUrl="submissions[0].imageUrl" -->
+<!-- :url="submissions[0]" -->
--- src/components/Upvote.vue
+++ src/components/Upvote.vue
@@ -1,10 +1,46 @@
 <template>
-  <div></div>
+  <article class="flex gap-3 border p-5 w-fit">
+    <figure>
+      <img class="w-[64px]" :src="submissionImage" />
+    </figure>
+    <div>
+      <div>
+        <p>
+          <strong>
+            <a href="#">{{ title }}</a>
+            <span>{{ id }}</span>
+          </strong>
+          <br />
+          {{ description }}
+          <br />
+          <small class="flex gap-3">
+            Submitted by:
+            <img class="w-[24px] rounded-xl" :src="avatar" />
+          </small>
+        </p>
+      </div>
+    </div>
+    <div>
+      <span>
+        <i class="fa fa-chevron-up"></i>
+        <strong>{{ votes }}</strong>
+      </span>
+    </div>
+  </article>
 </template>
 
 <script>
 export default {
   name: "Upvote",
+  props: [
+    "id",
+    "title",
+    "description",
+    "url",
+    "votes",
+    "avatar",
+    "submissionImage",
+  ],
 }
 </script>
 
